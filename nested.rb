cdded83f3ad30c85ca4ce725d2cc5838e9736c35
Da-mell Faines <damellfaines8@gmail.com>
2021-01-14 16:56:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,8 +104,11 @@ def changing_alan
         :languages => ["C"]
       }
     }
-    programmer_hash[:yukihiro_matsumoto]= { :known_for => "Ruby", :languages => ["LISP","C"] }
-      programmer_hash
+    programmer_hash[:yukihiro_matsumoto] = {
+ 		   :known_for => "Ruby",
+ 		   :languages => ["LISP, C"]
+ 		 }
+ 		 programmer_hash
 
 end
 
