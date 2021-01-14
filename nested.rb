f36e0215e98a8e5bfb5e1cc08ce877d24997695d
Da-mell Faines <damellfaines8@gmail.com>
2021-01-14 17:00:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,9 +108,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-    programmer_hash[:alan_kay][:known_for] = alans_new_info
-    alans_new_info = "GUI"
-		 programmer_hash
+  
 end
 
 def adding_to_dennis
