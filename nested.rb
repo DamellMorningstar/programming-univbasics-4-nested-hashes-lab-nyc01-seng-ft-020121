96d22f50b992a96daebc20eff814afb882be1c26
Da-mell Faines <damellfaines8@gmail.com>
2021-01-14 16:50:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,11 +66,8 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
+programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",:languages => ["LISP, C"]}
 
-programmer_hash.merge(:yukihiro_matsumoto => {
-   :known_for => "Ruby",
-    :languages => ["LISP, C"]
- })
 	programmer_hash =
  		{
       :grace_hopper => {
