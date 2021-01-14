dfccc750932a7fefa9ddf4575e34027ae3c6710a
Da-mell Faines <damellfaines8@gmail.com>
2021-01-14 16:55:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
       }
     }
     programmer_hash[:yukihiro_matsumoto]= { :known_for => "Ruby", :languages => ["LISP","C"] }
-     return programmer_hash
+      programmer_hash
 
 end
 
